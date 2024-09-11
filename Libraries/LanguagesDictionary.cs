f96ad6c117cf98504a22dd93441d42b030c61b6a
Mykhailo <mishamyrosh03@gmail.com>
2024-09-11 15:41:57 +0300

    Send request to API

---
+++ Libraries/LanguagesDictionary.cs
@@ -0,0 +1,47 @@
+﻿namespace Code_editor.Libraries
+{
+    public class LanguagesDictionary
+    {
+        private readonly Dictionary<string, string> languages = new Dictionary<string, string>
+        {
+            { "C", "C" },
+            { "C++14", "CPP14" },
+            { "C++17", "CPP17" },
+            { "Clojure", "CLOJURE" },
+            { "C#", "CSHARP" },
+            { "Go", "GO" },
+            { "Haskell", "HASKELL" },
+            { "Java 8", "JAVA8" },
+            { "Java 14", "JAVA14" },
+            { "JavaScript(Nodejs)", "JAVASCRIPT_NODE" },
+            { "Kotlin", "KOTLIN" },
+            { "Objective C", "OBJECTIVEC" },
+            { "Pascal", "PASCAL" },
+            { "Perl", "PERL" },
+            { "PHP", "PHP" },
+            { "Python 2", "PYTHON" },
+            { "Python 3", "PYTHON3" },
+            { "Python 3.8", "PYTHON3_8" },
+            { "R", "R" },
+            { "Ruby", "RUBY" },
+            { "Rust", "RUST" },
+            { "Scala", "SCALA" },
+            { "Swift", "SWIFT" },
+            { "TypeScript", "TYPESCRIPT" }
+        };
+
+        public List<string> GetLanguagesList()
+        {
+            return languages.Keys.ToList();
+        }
+
+        public string GetLanguageCode(string language)
+        {
+            if (languages.ContainsKey(language))
+            {
+                return languages[language];
+            }
+            return null;
+        }
+    }
+}
---
+++ Models/RequestBody.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace Code_editor.Models
+{
+    [Serializable]
+    public class RequestBody
+    {
+        [JsonProperty("source")]
+        [Required]
+        public string source { get; set; }
+     
+
+        [JsonProperty("lang")]
+        [Required]
+        public string lang { get; set; }
+
+        [JsonProperty("input")]
+        public string? Input { get; set; }
+
+        [JsonProperty("memory_limit")]
+        public int MemoryLimit { get; set; }
+
+        [JsonProperty("time_limit")]
+        public int TimeLimit { get; set; }
+
+        [JsonProperty("context")]
+        public string? Context { get; set; }
+        [JsonProperty("callback")]
+        public string? CallBack { get; set; }
+
+        public RequestBody(string sources, string langs, int memory_limit, int time_limit, string? input = null, string? context = null, string? callBack = null)
+        {
+            source = sources;
+            lang = langs;
+            MemoryLimit = memory_limit;
+            TimeLimit = time_limit;
+            Input = input;
+            Context = context;
+            CallBack = callBack;
+        }
+    }
+}
+
